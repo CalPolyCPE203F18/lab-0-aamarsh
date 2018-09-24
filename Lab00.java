eea74eba3b2251c3e8a5df8c03771f82fd97bcd2
aamarsh <aamarsh99@gmail.com>
2018-09-24 14:11:44 -0700

    updated

--- Lab00.java
+++ Lab00.java
@@ -2,7 +2,44 @@ public class Lab00
 {
    public static void main(String[] args)
    {
+      int x= 5;
+      String y= "hello";
+      double z = 9.8;
 
-   
+      System.out.println("x: " + x+ " y: "+ y+ " z: " +z);
+
+      int[] nums= {3, 6, -1, 2};
+
+
+      for(int i : nums)
+      {
+      	System.out.println(i);
+      }
+
+     int numFound = char_count(y, 'l');
+     System.out.println("Found: " + numFound);
+
+     for (int i =1; i<11; i++)
+     {
+     	System.out.print(i + " ");
+     }
+
+     System.out.println();
    }
+
+   public static int char_count(String s, char c)
+   {
+   	int count =0;
+   	char[] sArray = s.toCharArray();
+   	for (char ch: sArray)
+   	{
+   		if(ch == c)
+   		{
+   			count ++;
+   		}
+   		
+   	}
+   	return count;
+  	}
+
 }
